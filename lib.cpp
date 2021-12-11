96da03881e13152a8ab95252d7bcb3601d5f8ad0
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-11 18:35:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,13 @@
+#include <cmath>
+#include <limits>
+#include "lib.h"
 
+float max(int a,float numeri[]){
+    float m=-INFINITY;
+    for(int i=0; i<a;i++){
+        if(numeri[i]>m){
+            m=numeri[i];
+        }
+    }
+    return m;
+}

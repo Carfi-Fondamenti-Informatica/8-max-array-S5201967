55b3db3ef1dee5d7bc99c7c79efd0ead15bb9171
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-11 18:34:09 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int dimensione;
+    cin>>dimensione;
+    float array[dimensione];
+    fot(int i=0 ; i<dimensione;i++){
+        cin >> array[i]
+    }
+    float
+    ris=max(dimensione,array);
+    cout<<ris<<endl;
+    return 0;
 }
